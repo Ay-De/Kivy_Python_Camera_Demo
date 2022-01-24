f912535ad17f09e32dcc9b9d7edf4a14677cece2
Ay-De <91670696+Ay-De@users.noreply.github.com>
2022-01-24 23:17:17 +0100

    code updates

--- main.py
+++ main.py
@@ -1,15 +1,53 @@
 from kivy.utils import platform
 from kivy.app import App
-from kivy.uix.camera import Camera
+from kivy.uix.image import Image
+import cv2
+from kivy.clock import Clock
+from kivy.graphics.texture import Texture
+import cv2
 import time
+
 
 # Check if platform is android and import permissions for the popup
 if (platform == 'android'):
     import permissions
 
-class CamApp(Camera):
-    index = -1
-    captureResolution = (1280, 960)
+class CamApp(Image):
+
+    #Index of camera to use
+    index: int = 0
+    #Framerate per seconds at which the images should be drawn again
+    fps: int = 30
+
+    previewWidth: int = 960
+    previewHeight: int = 1280
+
+    def __init__(self, **kwargs):
+        super(CamApp, self).__init__(**kwargs)
+
+        #Connect CV2 to camera
+        self.imageStreamFromCamera = cv2.VideoCapture(self.index)
+
+        # create a texture with the same dimensions of the captured image.
+        # Will be used to display the image
+        self.texture = Texture.create(size=(self.previewWidth, self.previewHeight), colorfmt='bgr')
+
+        #Clock will call a function in a specified interval in seconds
+        Clock.schedule_interval(self._drawImage, (1.0/self.fps))
+
+
+    #Note: dt is not used, but required by kivys Clock.schedule_interval function
+    def _drawImage(self, dt):
+        #Get image from Camera
+        self._retval, self.frame = self.imageStreamFromCamera.read()
+        #self.frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
+
+        self.previewImage = cv2.resize(self.frame,
+                                       dsize=(self.previewWidth, self.previewHeight),
+                                       interpolation=cv2.INTER_LINEAR)
+
+        #Update the texture to display the actual image
+        self.texture.blit_buffer(self.previewImage.tostring(), colorfmt='bgr', bufferfmt='ubyte')
 
     def captureImage(self):
         image = self.ids['cameraPreview']
