e17f2b45b011df3c7e180a752be4297ded545996
Ay-De <91670696+Ay-De@users.noreply.github.com>
2022-01-25 01:29:22 +0100

    fixed preview orientation

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ from kivy.app import App
 from kivy.uix.image import Image
 from kivy.clock import Clock
 from kivy.graphics.texture import Texture
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.togglebutton import ToggleButton
 import cv2
 import time
-
 
 # Check if platform is android and import permissions for the popup
 if (platform == 'android'):
     import permissions
+
+class StartupScreen(RelativeLayout):
+    pass
 
 class CamApp(Image):
 
@@ -18,18 +22,14 @@ class CamApp(Image):
     #Framerate per seconds at which the images should be drawn again
     fps: int = 30
 
-    previewWidth: int = 960
-    previewHeight: int = 1280
+    previewWidth: int = 1280
+    previewHeight: int = 960
 
     def __init__(self, **kwargs):
         super(CamApp, self).__init__(**kwargs)
 
         #Connect CV2 to camera
         self.imageStreamFromCamera = cv2.VideoCapture(self.index)
-
-        # create a texture with the same dimensions of the captured image.
-        # Will be used to display the image
-        self.texture = Texture.create(size=(self.previewWidth, self.previewHeight), colorfmt='bgr')
 
         #Clock will call a function in a specified interval in seconds
         Clock.schedule_interval(self._drawImage, (1.0/self.fps))
@@ -41,9 +41,15 @@ class CamApp(Image):
         self._retval, self.frame = self.imageStreamFromCamera.read()
         #self.frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
 
+        self.frame = cv2.flip(self.frame, 0)
+
+        # create a texture with the same dimensions of the captured image.
+        # Will be used to display the image
+        self.texture = Texture.create(size=(self.previewWidth, self.previewHeight), colorfmt='bgr')
+
         self.previewImage = cv2.resize(self.frame,
                                        dsize=(self.previewWidth, self.previewHeight),
-                                       interpolation=cv2.INTER_LINEAR)
+                                       interpolation=cv2.INTER_AREA)
 
         #Update the texture to display the actual image
         self.texture.blit_buffer(self.previewImage.tostring(), colorfmt='bgr', bufferfmt='ubyte')
@@ -58,7 +64,7 @@ class CamApp(Image):
 
 class DemoApp(App):
     def build(self):
-        return CamApp()
+        return StartupScreen()
 
 if __name__ == '__main__':
     DemoApp().run()
