ad078c1be63d314fdcb41689a35258caa4505190
Ay-De <91670696+Ay-De@users.noreply.github.com>
2022-02-08 17:26:44 +0100

    fixed image preview in viewfinder and added button to save the image

--- source/main.py
+++ source/main.py
@@ -5,10 +5,11 @@ from kivy.lang.builder import Builder
 from kivy.clock import Clock
 from kivy.graphics.texture import Texture
 from kivy.uix.screenmanager import Screen, ScreenManager
-from kivy.uix.button import Button
+import threading
+from kivy.properties import ObjectProperty
 import cv2
 import time
-
+import numpy as np
 # Check if platform is android and import permissions for the popup
 if (platform == 'android'):
     import permissions
@@ -20,11 +21,16 @@ class MainPage(Image, Screen):
     #Framerate per seconds at which the images should be drawn again
     fps: int = 30
 
+    rawHeight = 1080
+    rawWidth = 1920
+    jpegQuality = 100 #in %
     previewHeight: int = 1280
     previewWidth: int = 960
 
     def __init__(self, **kwargs):
         super(MainPage, self).__init__(**kwargs)
+
+        self.texture = Texture.create(size=(self.previewWidth, self.previewHeight), colorfmt='bgr')
 
         #Connect CV2 to camera
         if (platform == 'android'):
@@ -33,39 +39,54 @@ class MainPage(Image, Screen):
             self.imageStreamFromCamera = cv2.VideoCapture(self.index, cv2.CAP_DSHOW)
             #self.imageStreamFromCamera.set(cv2.CAP_PROP_BUFFERSIZE, 2)
 
+        if self.imageStreamFromCamera.isOpened():
+            self.imageStreamFromCamera.set(cv2.CAP_PROP_FRAME_HEIGHT, self.rawHeight)
+            self.imageStreamFromCamera.set(cv2.CAP_PROP_FRAME_WIDTH, self.rawWidth)
         #Clock will call a function in a specified interval in seconds
-        Clock.schedule_interval(self._drawImage, int((1.0/self.fps)*1000))
+        Clock.schedule_interval(self._drawImage, (1.0/self.fps))
 
     #Note: dt is not used, but required by kivys Clock.schedule_interval function
     def _drawImage(self, dt):
         #Get image from Camera
-        self._retval, self.frame = self.imageStreamFromCamera.read()
+        _retval, self.frame = self.imageStreamFromCamera.read()
+
         #self.frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
 
         #Check if any frame was returned and if yes, process and display it
-        if self._retval:
-            if (platform == 'android'):
-                self.frame = cv2.flip(self.frame, 1)
-            else:
-                self.frame = cv2.flip(self.frame, 0)
+        #if _retval:
+        if (platform == 'android'):
+            self.frame = cv2.flip(self.frame, 1)
+        else:
+            self.frame = cv2.flip(self.frame, 0)
 
-            # create a texture with the same dimensions of the captured image.
-            # Will be used to display the image
-            self.texture = Texture.create(size=(self.previewWidth, self.previewHeight), colorfmt='bgr')
+        self.previewImage = cv2.resize(self.frame,
+                                       dsize=(self.previewWidth, self.previewHeight),
+                                       interpolation=cv2.INTER_AREA)
 
-            self.previewImage = cv2.resize(self.frame,
-                                           dsize=(self.previewWidth, self.previewHeight),
-                                           interpolation=cv2.INTER_AREA)
 
-            #Update the texture to display the actual image
-            self.texture.blit_buffer(self.previewImage.tostring(), colorfmt='bgr', bufferfmt='ubyte')
-            self.ids.cameraPreview = self.texture
+        #Update the texture to display the actual image
+        self.texture.blit_buffer(self.previewImage.tostring(), colorfmt='bgr', bufferfmt='ubyte')
+        self.canvas.ask_update()
+        #self.disp = self.ids.cameraPreview.canvas.get_group('display')[0]
+        #self.ids.saveImgBtn.canvas.ask_update()
+        #self.disp.texture = self.texture
+        #self.disp = self.ids.cameraPreview.canvas.clear()
+        #self.disp.size = (self.previewWidth, self.previewHeight)
+       # self.disp.texture = self.texture
+        #self.ids.cameraPreview.canvas.ask_update()
+        #self.ids.cameraPreview.texture = self.texture
 
     def captureImage(self):
-        self.image = cv2.flip(self.frame, 0)
+
         self.timeStamp = time.strftime('%Y%m%d_%H%M%S')
-        cv2.imwrite('IMG_{}.jpg'.format(self.timeStamp), self.image)
-        print('image saved')
+      #  thread = threading.Thread(target=cv2.imwrite,
+      #                            args=[f'IMG_{self.timeStamp}.jpg',
+      #                                  self.frame,
+      #                                  [int(cv2.IMWRITE_JPEG_QUALITY), self.jpegQuality]])
+      #  thread.start()
+
+        cv2.imwrite(f'IMG_{self.timeStamp}.jpg', self.frame, [int(cv2.IMWRITE_JPEG_QUALITY), self.jpegQuality])
+
 
 class SettingsPage(Screen):
     pass
@@ -74,10 +95,9 @@ class WindowManager(ScreenManager):
     pass
 
 
-kv = Builder.load_file('layout.kv')
-
 class DemoApp(App):
     def build(self):
+        kv = Builder.load_file('layout.kv')
         return kv
 
 if __name__ == '__main__':
