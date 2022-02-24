da70688c3c6d5080c739e68c4e93728df7dcc0f0
Ay-De <91670696+Ay-De@users.noreply.github.com>
2022-02-24 22:21:12 +0100

    cleaned up the code a bit. Updated readme and roadmap.

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# Kivy_Python_Camera_Demo
-Demo application of using the built in Camera in Windows or Android. GUI is created with Kivy, communication with the camera is accomplished with OpenCV and Python.
-The Android App will be built with buildozer.
+# Kivy_Python_Camera
+A simple camera application built to run on Windows or Android. The GUI is created with Kivy, communication with the camera is accomplished with OpenCV and Python.
+The Android App is built with buildozer.
+
+This is more of a proof of work concept demo than an actual camera application intended to be used as a daily driver.
 
 ## Development Roadmap
 
@@ -9,9 +11,9 @@ The Android App will be built with buildozer.
 | Create Github repo | ✅|
 | Add basic Camera feed | ✅|
 | Build the App with Buildozer | ✅|
-| Create a basic GUI and preview Viewfinder | ⚠️|
+| Create a basic GUI and preview Viewfinder | ✅|
 | Add a Button to switch between Camera lenses | ❌ |
-| Add a Settings Page with some Options like image Resolution, and so on | ❌ |
+| Add a Settings Page with some Options like image Resolution, and so on | ⚠️|
 | Try out direct API Calls from Python to Android | ❌ |
 | Add Github workflow to build new APKs | ✅|
 | Add Github workflow to sign build APKs | ❌ |
@@ -22,9 +24,13 @@ Legend:
 ⚠️in progress
 ❌ not started yet
 
-## More Info coming soon
+## Known Issues
+
+- Ultrawide camera lenses are producing a black image in the viewfinder and no image is saved if the shutter button is pressed
+- After switching between more than 2 camera lenses, the viewfinder becomes quite slow
 
 ## Used libraries
+
 https://github.com/kivy/kivy
 
 https://github.com/opencv/opencv
--- source/main.py
+++ source/main.py
@@ -6,51 +6,46 @@ from kivy.clock import Clock
 from kivy.graphics.texture import Texture
 from kivy.uix.screenmanager import Screen, ScreenManager
 import threading
-from kivy.properties import ObjectProperty
 import cv2
 import time
-import numpy as np
 import os
 from pathlib import Path
 from itertools import cycle
 
-#from jnius import autoclass
-
-# Check if platform is android and import permissions for the popup
+#Check if platform is android and import permissions for the popup
 if (platform == 'android'):
+    #Get path to internal Android Storage
     from android.storage import primary_external_storage_path
     import permissions
 
-
+#This class will contain the user changable settings.
+#I.e. JPEG Quality, Saving directory for the images,...
 class Settings:
 
     jpegQuality = 98
 
 
+#This class is part of the main page, containing the shutterbutton and image preview viewfinder
 class MainPage(Image, Screen, Settings):
-
-   # if (platform == 'android') and (platform == 'windows'):
-   #     Camera = autoclass('android.hardware.Camera')
-   #     try:
-   #         co = Camera.open(int(0))
-   #         x = co.getParameters().getSupportedPictureSizes()
-   #         print(x)
-   #         co.release()
-   #     except:
-   #         print('could not open camera')
 
     def __init__(self, nextLens=0, **kwargs):
         super(MainPage, self).__init__(**kwargs)
 
-        # Framerate per seconds at which the images should be drawn again
+        #Framerate per seconds at which the images should be drawn again
         self.fps = 30
 
-        # Setting raw sensor height and width to 10000, to get the highest possible resolution.
+        #Setting raw sensor height and width to 10000, to get the highest possible resolution.
         self.rawHeight = 10000
         self.rawWidth = 10000
 
-        #self.jpegQuality = 100  # in %
+        #Set storage path for the captured photos according to the used platform
+        if (platform == 'android'):
+            self.downloadDir = os.path.join(primary_external_storage_path(), 'Download')
+        else:
+            self.downloadDir = str(Path.home() / 'Downloads')
 
+        #On android, switch width and height, because pictures on phones are usually taken
+        #vertically, instead of horizontally like on tablets or laptops
         if platform == 'win':
             self.previewHeight = 940
             self.previewWidth = 1280
@@ -60,10 +55,11 @@ class MainPage(Image, Screen, Settings):
 
         self.texture = Texture.create(size=(self.previewWidth, self.previewHeight), colorfmt='rgb')
 
+        #all possible lens id's will be stored here
         self.lenses = []
 
-        # Get number of cameras
-        for n in range(0, 5):
+        #Get id of each usable camera
+        for n in range(0, 6):
             self._tempCam = cv2.VideoCapture(n,
                                              cv2.CAP_DSHOW) if (platform == 'win') else cv2.VideoCapture(n,
                                                                                                             cv2.CAP_ANDROID)
@@ -76,45 +72,42 @@ class MainPage(Image, Screen, Settings):
                 self._tempCam.release()
                 break
 
+        #for convenience, make it possible to cycle through the list of lens id's
         self.cyclelens = cycle(self.lenses)
 
-        print(self.lenses)
-
-
+        #call the module to initialize the camera lens
         self._switch(nextLens)
 
+    #Module to connect to the lens. default and starting value is 0
     def _switch(self, nextLens):
-        print('nextLens in reset:', nextLens)
-        # Index of camera to use
+        #Index of camera to use
         self.index = nextLens
 
+        #Sync the the next camera lens with the cyclelens output
+        ######### NOTE: CHECK IF NECESSARY. SHOULD BE REDUNDANT NOW ##################
         while (nextLens != next(self.cyclelens)):
             next(self.cyclelens)
-            print(next(self.cyclelens))
 
-        # Connect CV2 to camera
+        #Connect opencv to camera
         if (platform == 'android'):
             self.imageStreamFromCamera = cv2.VideoCapture(self.index, cv2.CAP_ANDROID)
-            self.downloadDir = os.path.join(primary_external_storage_path(), 'Download')
         else:
-            self.downloadDir = str(Path.home() / 'Downloads')
             self.imageStreamFromCamera = cv2.VideoCapture(self.index, cv2.CAP_DSHOW)
 
-
+        #Set camera to max resultion if the connection was successful
         if self.imageStreamFromCamera.isOpened():
             self.imageStreamFromCamera.set(cv2.CAP_PROP_FRAME_WIDTH, self.rawWidth)
             self.imageStreamFromCamera.set(cv2.CAP_PROP_FRAME_HEIGHT, self.rawHeight)
-            print('height', self.imageStreamFromCamera.get(cv2.CAP_PROP_FRAME_HEIGHT))
-            print('width', self.imageStreamFromCamera.get(cv2.CAP_PROP_FRAME_WIDTH))
+
 
             self.imageStreamFromCamera.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('R','G','B','3'))
             self.imageStreamFromCamera.set(cv2.CAP_PROP_FPS, self.fps)
             self.imageStreamFromCamera.set(cv2.CAP_PROP_BUFFERSIZE, 3)
 
-        # Clock will call a function in a specified interval in seconds
+        #Clock will call a function in a specified interval in seconds
         Clock.schedule_interval(self._drawImage, (1.0 / self.fps))
 
-   #Note: dt is not used, but required by kivys Clock.schedule_interval function
+    #Note: dt is not used, but required by kivys Clock.schedule_interval function
     def _drawImage(self, dt):
 
         #Get image from Camera
@@ -122,13 +115,16 @@ class MainPage(Image, Screen, Settings):
 
         #Check if any frame was returned and if yes, process and display it
         if _retval:
+
+            #Flip and rotate image until the orientation is correct
+            ############## NOTE: ADD SETTINGS OPTION TO MANUALLY LET IT BE CORRECTED BY THE USER ##################
+            ####### DIFFERENT DEVICES USE DIFFERENT ORIENTATION ###################
             if (platform == 'win'):
                 self.frame = cv2.flip(self.frame, 0)
                 self.frame = cv2.cvtColor(self.frame, cv2.COLOR_RGB2BGR)
             else:
                 self.frame = cv2.rotate(self.frame, cv2.ROTATE_90_CLOCKWISE)
                 self.frame = cv2.flip(self.frame, 0)
-                #self.frame = cv2.cvtColor(self.frame, cv2.COLOR_RGB2BGR)
 
             self.previewImage = cv2.resize(self.frame,
                                         dsize=(self.previewWidth, self.previewHeight),
@@ -138,32 +134,41 @@ class MainPage(Image, Screen, Settings):
             self.texture.blit_buffer(self.previewImage.tostring(), colorfmt='rgb', bufferfmt='ubyte')
             self.canvas.ask_update()
 
+    #This function will save the image if the shutter button has been pressed
     def captureImage(self):
 
+        #Save the image and timestamp in a new variable as a first step. Time critical in image capturing
         self._image = self.frame
         self._timeStamp = time.strftime('%Y%m%d_%H%M%S')
 
+        #Again, correct the image orientation before saving
         if platform == 'win':
             self._image = cv2.flip(cv2.cvtColor(self._image, cv2.COLOR_RGB2BGR), 0)
         else:
             self._image = cv2.flip(cv2.cvtColor(self._image, cv2.COLOR_RGB2BGR), 0)
 
-        print(Settings.jpegQuality)
+        #Use a new thread to save the image in the background while displaying the next frames in the viewfinder
         thread = threading.Thread(target=cv2.imwrite,
                                   args=[os.path.join(self.downloadDir, f'IMG_{self._timeStamp}.jpg'),
                                         self._image,
                                         [int(cv2.IMWRITE_JPEG_QUALITY), Settings.jpegQuality]])
         thread.start()
 
+    #This function will switch between the lenses
     def switchLens(self):
+        #Stop the opencv connection to the camera
         self.imageStreamFromCamera.release()
 
+        #Get the next lens id which should be activated if the user presses on the button "switch"
         self.activeLens = next(self.cyclelens)
 
-        #Reload the Class with the new Camera Lens
+        #Reconnect opencv to the next camera lens
         MainPage._switch(self, self.activeLens)
 
 
+#This class is part of the second page, the settings page, where the user can make some adjustments.
+#All settings will be applied once the user presses on the Settings icon in the upper left corner
+#to go back to the viewfinder image.
 class SettingsPage(Screen, Settings):
 
     def setSettings(self):
